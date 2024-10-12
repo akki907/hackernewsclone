f50a8a185a0ebb4c6277ab5b60251c8cbc1700cc
Akash kumar <ak.akki907@gmail.com>
2024-10-12 10:22:21 +0530

    drizzle setup

---
+++ server/adapter.ts
@@ -0,0 +1,31 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+
+import postgres from "postgres";
+import { z } from "zod";
+
+// import { sessionTable, userRelations, userTable } from "./db/schemas/auth";
+// import { commentRelations, commentsTable } from "./db/schemas/comments";
+// import { postsRelations, postsTable } from "./db/schemas/posts";
+// import {
+//   commentUpvoteRelations,
+//   commentUpvotesTable,
+//   postUpvoteRelations,
+//   postUpvotesTable,
+// } from "./db/schemas/upvotes";
+
+const EnvSchema = z.object({
+  DATABASE_URL: z.string().url(),
+});
+
+const processEnv = EnvSchema.parse(process.env);
+
+const queryClient = postgres(processEnv.DATABASE_URL);
+export const db = drizzle(queryClient, {
+  schema: {
+   
+  },
+});
+
+// export const adapter = new DrizzlePostgreSQLAdapter(
+//   db,
+// );
