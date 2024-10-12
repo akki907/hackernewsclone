0154097df4e48d5c870583989643a6eb9e5549b6
Akash kumar <ak.akki907@gmail.com>
2024-10-12 10:36:43 +0530

    error setuo

--- server/adapter.ts
+++ server/adapter.ts
@@ -20,11 +20,7 @@ const EnvSchema = z.object({
 const processEnv = EnvSchema.parse(process.env);
 
 const queryClient = postgres(processEnv.DATABASE_URL);
-export const db = drizzle(queryClient, {
-  schema: {
-   
-  },
-});
+export const db = drizzle(queryClient);
 
 // export const adapter = new DrizzlePostgreSQLAdapter(
 //   db,
